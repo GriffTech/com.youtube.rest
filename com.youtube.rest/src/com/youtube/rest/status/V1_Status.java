a7291a0c27068af8b9118325a38d93042a17e9e9
GriffTech <GriffTechs@gmail.com>
2015-02-11 19:01:06 +0000

    11/02/2015 - Basic REST Service now working

--- com.youtube.rest/src/com/youtube/rest/status/V1_Status.java
+++ com.youtube.rest/src/com/youtube/rest/status/V1_Status.java
@@ -1,21 +1,40 @@
 package com.youtube.rest.status;
 
 import javax.ws.rs.core.MediaType;
-
 import javax.ws.rs.*;
 
 
-@Path("/v1/status") //removed * wildcard to make this more compatible with tomcat
+@Path("/v1/status/") //removed * wildcard to make this more compatible with tomcat & glassfish
 public class V1_Status {
 
-	//private static final String api_version = "00.02.00"; //version of the api
+	private static final String api_version = "00.02.00"; //version of the api
 	
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String returnTitle() {
-		return "<p>Java Web Service</p>";
+		return "<p><b>My First Java Web Service</b></p>";
 	}
 	
-
+	@Path("/version") 
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion() {
+		
+		return "<b>Version: </b>"+ api_version;
+	}
+	
+	@Path("/details") 
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnDetails() {
+		return "<p><b>These are the main details</b></p>";
+	}
+	
+	@Path("/otherdetails") 
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnOtherDetails() {
+		return "<p><b>These are the other details</b></p>";
+	}
 	
 }
